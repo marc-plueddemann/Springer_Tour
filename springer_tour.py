ab21ae8583ff810eb9d2976bda5f826e98d0d473
Marc <marc@plueddemann.de>
2023-06-26 17:38:39 +0200

    small fix

--- springer_tour.py
+++ springer_tour.py
@@ -3,21 +3,27 @@ x = 'x'
 
 # board construction ([[]], start x, start y, starting number) 
 
+# takes too long, dont try
 board1 =  ([[0,x,4,x,x,x],
             [3,x,1,x,5,x],
             [x,29,x,7,x,x],
             [x,2,x,x,x,6],
             [x,x,x,x,x,x]], 2, 3, 7)
 
+# takes too long, dont try
 board2 =  ([[0,x,x,x,x,x],
             [x,x,1,x,x,x],
             [x,x,x,x,x,x],
             [x,2,x,x,x,x],
             [x,x,x,x,x,x]], 2, 3, 2)
 
+# produces the output seen in empty_field_5x6.txt after about 1 minute
 board3 =  ([[0,x,x,x,x,x],[x,x,x,x,x,x],[x,x,x,x,x,x],[x,x,x,x,x,x],[x,x,x,x,x,x]], 0, 0, 0)
 
+# no possible solution
 board4 = ([[0,x,x,x],[x,x,x,x],[x,x,x,x],[x,x,x,x]], 0, 0, 0)
+
+# 5x5 tours should be possible and problably the best way to test...
 
 
 def fill_board(boardstate, debug=False):
@@ -25,10 +31,10 @@ def fill_board(boardstate, debug=False):
     to find a closed loop add the last entry to the board manually'''
 
     (board, px, py, count) = boardstate
-    if debug: print(board)
+    if debug: print('current board:', board)
 
     count += 1
-    if debug: print(count)
+    if debug: print('current search for:', count)
 
     # stop algorithm if last number of board is found (goes backwards recursive from here)
     if count == (len(board) * len(board[0])): 
@@ -45,7 +51,7 @@ def fill_board(boardstate, debug=False):
         if debug: print(f'x={px}, y={py}, x+a={px+a}, y+b={py+b}, a={a}, b={b}, Count={count}, State={i}')
 
         # skip loop if index out of bounds
-        if ((px+a > (len(board[0])-1)) or (px+a < 0)) or (py+b > (len(board)-1) or py+b < 0): 
+        if ((px+a > (len(board)-1)) or (px+a < 0)) or (py+b > (len(board[0])-1) or py+b < 0): 
             if debug: print('out of index')
             continue
 
@@ -78,9 +84,4 @@ def fill_board(boardstate, debug=False):
 
     if debug: print('exit at ' + str(count))
 
-fill_board(board4, debug=True)
-
-# %%
-x = 3
-y = 1
-print(board[x][y])
+fill_board(board2, debug=True)
